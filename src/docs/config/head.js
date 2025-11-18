a1468e61312cf3cc5e83878183a893892430ba77
Pablo Smolak <pablosmolak2004@hotmail.com>
2025-11-17 21:46:42 -0400

    corrigindo a documentação e adicionando a licença do software

--- src/docs/config/head.js
+++ src/docs/config/head.js
@@ -24,121 +24,125 @@ import { GrupoPath } from "../paths/grupoPath.js";
 import { GrupoSchemas } from "../schemas/gruposSchema.js";
 
 const swaggerOptions = {
-  swaggerDefinition: {
-    openapi: "3.0.0",
-    info: {
-      title: "API - Academia FSlab",
-      description:
-        "A api foi desenvolvida para a plataforma de curso Academia FSlab.",
-      version: "0.0.1",
-      license: {
-        name: "GPLv3",
-        url: "http://www.gnu.org/licenses/gpl-3.0.html",
-      },
-    },
-    externalDocs: {
-      description: "Documentação detalhada",
-      url: "",
-    },
-    servers: [
-      {
-        url: `https://api-academia.app.fslab.dev/`,
-        description: "API em produção",
-      },
-      {
-        url: `http://localhost:${process.env.PORT}`,
-        description: "API em desenvolvimento",
-      }
-    ],
-    components: {
-      securitySchemes: {
-        jwtAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT'
+    swaggerDefinition: {
+        openapi: "3.0.0",
+        info: {
+            title: "API - Academia FSlab",
+            description: "API RESTful desenvolvida para a plataforma de cursos Academia FSlab.",
+            version: "0.0.1",
+            contact: {
+                name: "Pablo Smolak",
+                email: "pablosmolak@gmail.com",
+                url: "https://github.com/pablosmolak"
+            },
+            license: {
+                name: "MIT",
+                url: "https://opensource.org/licenses/MIT",
+            },
         },
-      },
-      schemas: {
-        ...UsuarioSchemas,
-        ...GrupoSchemas,
-        ...CategoriaSchemas,
-        ...CursoSchemas,
-        ...TopicoSchemas,
-        ...ConteudoCursoSchemas,
-        ...InscricaoSchemas,
-        ...ProgressoSchemas,
-        ...CertificadoSchemas,
-        ...VerificarEmailSchemas,
-        ...AuthSchemas,
-        ...RecuperarSenhaSchema
-      },
+        externalDocs: {
+            description: "Documentação detalhada",
+            url: "",
+        },
+        servers: [
+            {
+                url: `https://api-academia.app.fslab.dev`,
+                description: "API em produção",
+            },
+            {
+                url: `http://localhost:${process.env.PORT}`,
+                description: "API em desenvolvimento",
+            }
+        ],
+        components: {
+            securitySchemes: {
+                jwtAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT'
+                },
+            },
+            schemas: {
+                ...UsuarioSchemas,
+                ...GrupoSchemas,
+                ...CategoriaSchemas,
+                ...CursoSchemas,
+                ...TopicoSchemas,
+                ...ConteudoCursoSchemas,
+                ...InscricaoSchemas,
+                ...ProgressoSchemas,
+                ...CertificadoSchemas,
+                ...VerificarEmailSchemas,
+                ...AuthSchemas,
+                ...RecuperarSenhaSchema
+            },
+        },
+        tags: [
+            {
+                name: "Login",
+                description: "Autenticação do usuário no sistema"
+            },
+            {
+                name: "Recuperar Senha",
+                description: "Processo de recuperação de senha do usuário"
+            },
+            {
+                name: "Verificação de Email",
+                description: "Processo de verificação do e-mail do usuário"
+            },
+            {
+                name: "Usuários",
+                description: "Gerenciamento de usuários do sistema"
+            },
+            {
+                name: "Grupos",
+                description: "Gerenciamento de grupos do sistema"
+            },
+            {
+                name: "Categorias",
+                description: "Gerenciamento das categorias de cursos"
+            },
+            {
+                name: "Cursos",
+                description: "Gerenciamento e informações sobre os cursos"
+            },
+            {
+                name: "Tópicos",
+                description: "Gerenciamento e informações sobre os tópicos dos cursos"
+            },
+            {
+                name: "Conteúdos",
+                description: "Gerenciamento e informações sobre os conteúdo dos tópicos dos cursos"
+            },
+            {
+                name: "Inscrições",
+                description: "Gerenciamento das inscrições nos cursos"
+            },
+            {
+                name: "Progressos",
+                description: "Gerenciamento do progresso dos estudantes nos cursos"
+            },
+            {
+                name: "Certificados",
+                description: "Gerenciamento do certificado dos estudantes nos cursos"
+            }
+        ],
+        paths: {
+            ...AuthPath,
+            ...UsuarioPath,
+            ...GrupoPath,
+            ...CategoriaPath,
+            ...CursoPath,
+            ...TopicoPath,
+            ...RecuperaSenhaPath,
+            ...ConteudoPath,
+            ...InscricaoPath,
+            ...ProgressoPath,
+            ...CertificadoPath,
+            ...VerificarEmailPath
+        },
     },
-    tags: [
-      {
-        name: "Login",
-        description: "Autenticação do usuário no sistema"
-      },
-      {
-        name: "Recuperar Senha",
-        description: "Processo de recuperação de senha do usuário"
-      },
-      {
-        name: "Verificação de Email",
-        description: "Processo de verificação do e-mail do usuário"
-      },
-      {
-        name: "Usuários",
-        description: "Gerenciamento de usuários do sistema"
-      },
-      {
-        name: "Grupos",
-        description: "Gerenciamento de grupos do sistema"
-      },
-      {
-        name: "Categorias",
-        description: "Gerenciamento das categorias de cursos"
-      },
-      {
-        name: "Cursos",
-        description: "Gerenciamento e informações sobre os cursos"
-      },
-      {
-        name: "Tópicos",
-        description: "Gerenciamento e informações sobre os tópicos dos cursos"
-      },
-      {
-        name: "Conteúdos",
-        description: "Gerenciamento e informações sobre os conteúdo dos tópicos dos cursos"
-      },
-      {
-        name: "Inscrições",
-        description: "Gerenciamento das inscrições nos cursos"
-      },
-      {
-        name: "Progressos",
-        description: "Gerenciamento do progresso dos estudantes nos cursos"
-      },
-      {
-        name: "Certificados",
-        description: "Gerenciamento do certificado dos estudantes nos cursos"
-      }
-    ],
-    paths: {
-      ...AuthPath,
-      ...UsuarioPath,
-      ...GrupoPath,
-      ...CategoriaPath,
-      ...CursoPath,
-      ...TopicoPath,
-      ...RecuperaSenhaPath,
-      ...ConteudoPath,
-      ...InscricaoPath,
-      ...ProgressoPath,
-      ...CertificadoPath,
-      ...VerificarEmailPath
-    },
-  },
-  apis: ["./src/routes/*.js"],
+    apis: ["./src/routes/*.js"],
 }
 
 export default swaggerOptions;
